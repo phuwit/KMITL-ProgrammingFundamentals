8817fdc08cd8fb7eabd8e768b1c9ad821669a278
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-08-21 20:30:27 +0700

    lab6 done

--- Labs/Lab7/lab6.cpp
+++ Labs/Lab7/lab6.cpp
@@ -1,15 +1,18 @@
-#include <stdio.h>
 #include <conio.h>
+#include <stdio.h>
 #include <time.h>
 #include <windows.h>
 
 void setcursor(bool visible);
 void setcolor(int fg, int bg);
 void gotoxy(int x, int y);
+char cursor(int x, int y);
 void draw_ship(int x, int y);
 void erase_ship(int x, int y);
 void draw_bullet(int x, int y);
 void erase_bullet(int x, int y);
+void draw_random_stars(int starCount);
+void display_score(int score, char* scoreIncrease);
 
 const int SHIP_LENGTH = 7;
 const int SHIP_WIDTH = 1;
@@ -24,6 +27,8 @@ int main() {
     setcursor(false);
     setcolor(0, 0);
 
+    srand(time(0));
+
     char ch = '\0';
     char lastPressed = ch;
     int x = SCREEN_WIDTH - (SHIP_LENGTH / 2), y = 20;
@@ -33,8 +38,12 @@ int main() {
         bulletsY[i] = -1;
     }
     int bulletsInFlight = 0;
+
+    int score = 0;
+    char scoreIncrease = 'F';
+
     draw_ship(x, y);
-    // move left/ right
+    draw_random_stars(20);
     do {
         // handle input
         if (_kbhit()) {
@@ -58,11 +67,27 @@ int main() {
         if (bulletsInFlight > 0) {
             for (int i = 0; i < MAX_BULLETS; i++) {
                 if (bulletsX[i] != -1 && bulletsY[i] != -1) {
-                    erase_bullet(bulletsX[i], bulletsY[i]);
-                    bulletsY[i]--;
-                    draw_bullet(bulletsX[i], bulletsY[i]);
+                    // check collisions
+                    if (cursor(bulletsX[i], bulletsY[i] - 1) == '*') {
+                        // hit a star, remove a bullet
+                        Beep(1500, 200);
+                        score++;
+                        scoreIncrease = 'T';
+                        draw_random_stars(1);
+                        // remove star and bullet
+                        erase_bullet(bulletsX[i], bulletsY[i] - 1);
+                        erase_bullet(bulletsX[i], bulletsY[i]);
+                        bulletsX[i] = -1;
+                        bulletsY[i] = -1;
+                        bulletsInFlight--;
+                    } else {
+                        erase_bullet(bulletsX[i], bulletsY[i]);
+                        bulletsY[i]--;
+                        draw_bullet(bulletsX[i], bulletsY[i]);
+                    }
                 }
 
+                // remove OOB bullet
                 if (bulletsY[i] == 0) {
                     erase_bullet(bulletsX[i], bulletsY[i]);
                     bulletsX[i] = -1;
@@ -84,6 +109,8 @@ int main() {
             erase_ship(x, y);
             draw_ship(x, ++y);
         }
+
+        display_score(score, &scoreIncrease);
 
         Sleep(50);
         // exit
@@ -109,6 +136,18 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
+char cursor(int x, int y) {
+    HANDLE hStd = GetStdHandle(STD_OUTPUT_HANDLE);
+    char buf[2];
+    COORD c = {(short)x, (short)y};
+    DWORD num_read;
+    if (!ReadConsoleOutputCharacter(hStd, (LPTSTR)buf, 1, c,
+                                    (LPDWORD)&num_read))
+        return '\0';
+    else
+        return buf[0];
+}
+
 void draw_ship(int x, int y) {
     // set a color to green, red
     setcolor(2, 4);
@@ -128,6 +167,8 @@ void draw_bullet(int x, int y) {
     setcolor(9, 0);
     gotoxy(x, y);
     printf("|");
+    // Assignment 2
+    // Beep(400, 20);
 }
 
 void erase_bullet(int x, int y) {
@@ -135,4 +176,30 @@ void erase_bullet(int x, int y) {
     setcolor(0, 0);
     gotoxy(x, y);
     printf(" ");
+}
+
+// lab7 assn1
+void draw_random_stars(int starCount) {
+    setcolor(7, 0);
+    for (int i = 0; i < starCount; i++) {
+        int y = rand() % 4 + 2;
+        int x = rand() % 61 + 10;
+        if (cursor(x, y) != '*') {
+            gotoxy(x, y);
+            printf("*");
+        } else {
+            i--;
+        }
+    }
+}
+
+void display_score(int score, char* scoreIncrease) {
+    gotoxy(SCREEN_WIDTH - 12, 0);
+    if (*scoreIncrease == 'T') {
+        setcolor(14, 0);
+        *scoreIncrease = 'F';
+    } else {
+        setcolor(6, 0);
+    }
+    printf("Score : %.4d", score);
 }
